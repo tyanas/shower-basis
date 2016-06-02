8de5f5fc3c562bab5bfca6971f5ed27eceaa57cc
tyanas <tyanas.go@gmail.com>
2016-06-02 10:25:53 +0300

    mimic ribbon

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 basis.ready(function(){
     var Node = basis.require('basis.ui').Node;
-    var dict = require('basis.l10n').dictionary('./slides.l10n');
+    var Slide = basis.require('./slide.js');
+    var config = basis.require('./ribbon-theme-example.js');
     var l10n = basis.require('basis.l10n');
 
     l10n.setCultureList('en-US ru-RU');
-    l10n.setCulture('ru-RU');
+    l10n.setCulture('en-US');
 
     new Node({
         container: document.body,
         template: resource('./template/view.tmpl'),
-        childClass: {
-            template: resource('./template/slide.tmpl'),
-            header: null,
-            shout: false,
-            shrink: false,
-            binding: {
-                id: 'id',
-                header: 'header',
-                shout: 'shout',
-                shrink: 'shrink',
-                key: 'key'
-            }
+        showProgressBar: true,
+        binding: {
+            showProgressBar: 'showProgressBar'
         },
-        childNodes: [
-            {
-                shout: true,
-                header: dict.token('s1.header.caption'),
-                key: 's1'
-            },
-            {
-                key: 's3'
-            },
-            {
-                id: 'loud',
-                shout: true,
-                shrink: true,
-                key: 'loud',
-                header: dict.token('loud.header.caption'),
-            }
-        ]
+        childClass: Slide,
+        childNodes: config
     });
 });
